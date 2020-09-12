ac1e129e5775c7d22110d6ef0c57ec1343da2c2e
Mat Sz <contact@matsz.dev>
2020-09-12 17:26:02 +0200

    added a Home page component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.scss';
 
+import Home from './pages/Home';
+
 const App: React.FC = () => {
   return (
     <Router>
       <div className="app">
         <Switch>
           <Route path="/">
-            <h1>Hello, world!</h1>
+            <Home />
           </Route>
         </Switch>
       </div>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Board from '../components/Board';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>2048</h1>
+      <Board />
+    </div>
+  );
+};
+
+export default Home;

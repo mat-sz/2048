06934e29c644a37b5dcbf3ba0ded0a246c132336
Mat Sz <contact@matsz.dev>
2020-09-14 18:39:28 +0200

    renamed box to tile

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -8,8 +8,8 @@ const Board: React.FC<BoardProps> = ({ boardSize }) => {
   return (
     <div className="board" style={{ '--board-size': boardSize } as any}>
       {new Array(Math.pow(boardSize, 2)).fill(undefined).map((_, i) => (
-        <div className="board-box board-box-empty" key={i}>
-          <div className="board-box-text">2</div>
+        <div className="board-tile board-tile-empty" key={i}>
+          <div className="board-tile-text">2</div>
         </div>
       ))}
     </div>

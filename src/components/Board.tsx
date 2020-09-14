27ffeb27ebd5951a98986f070d1bc46b362ad0c8
Mat Sz <contact@matsz.dev>
2020-09-14 18:39:11 +0200

    added styles for tile text

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -8,7 +8,9 @@ const Board: React.FC<BoardProps> = ({ boardSize }) => {
   return (
     <div className="board" style={{ '--board-size': boardSize } as any}>
       {new Array(Math.pow(boardSize, 2)).fill(undefined).map((_, i) => (
-        <div className="board-box board-box-empty" key={i}></div>
+        <div className="board-box board-box-empty" key={i}>
+          <div className="board-box-text">2</div>
+        </div>
       ))}
     </div>
   );

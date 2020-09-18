7d4be1fd5952d6c65ea73d61b3dd13771bd095c4
Mat Sz <contact@matsz.dev>
2020-09-18 18:14:26 +0200

    added a keydown listener

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 import Board from '../components/Board';
+import { moveAction } from '../actions';
+import { Direction } from '../types/Direction';
 
 const Home: React.FC = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const keydownListener = (e: KeyboardEvent) => {
+      switch (e.key) {
+        case 'ArrowDown':
+          dispatch(moveAction(Direction.DOWN));
+          break;
+        case 'ArrowUp':
+          dispatch(moveAction(Direction.UP));
+          break;
+        case 'ArrowLeft':
+          dispatch(moveAction(Direction.LEFT));
+          break;
+        case 'ArrowRight':
+          dispatch(moveAction(Direction.RIGHT));
+          break;
+      }
+    };
+
+    window.addEventListener('keydown', keydownListener);
+
+    return () => {
+      window.removeEventListener('keydown', keydownListener);
+    };
+  }, [dispatch]);
+
   return (
     <div className="page">
       <h1>2048</h1>

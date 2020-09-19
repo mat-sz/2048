bc77c00b421d277debb2454c6e359a00eabb3738
Mat Sz <contact@matsz.dev>
2020-09-19 15:06:32 +0200

    added a new game button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { resetAction } from '../actions';
 import { StateType } from '../reducers';
 
 const Header: React.FC = () => {
   const score = useSelector((state: StateType) => state.score);
+  const dispatch = useDispatch();
+  const reset = useCallback(() => dispatch(resetAction()), [dispatch]);
 
   return (
     <div className="header">
-      <h1>2048</h1>
-      <div className="header-scores">
-        <div className="header-scores-score">
-          <div>Score</div>
-          <div>{score}</div>
+      <div className="header-row">
+        <h1>2048</h1>
+        <div className="header-scores">
+          <div className="header-scores-score">
+            <div>Score</div>
+            <div>{score}</div>
+          </div>
+        </div>
+      </div>
+      <div className="header-row">
+        <div>
+          Join the numbers and get to the <strong>2048 tile!</strong>
+        </div>
+        <div>
+          <button onClick={reset}>New game</button>
         </div>
       </div>
     </div>

a097f8f97c37798016093cec668ae964a0f235d5
Mat Sz <contact@matsz.dev>
2020-09-19 14:59:08 +0200

    added score display

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { StateType } from '../reducers';
 
 const Header: React.FC = () => {
+  const score = useSelector((state: StateType) => state.score);
+
   return (
     <div className="header">
       <h1>2048</h1>
+      <div className="header-scores">
+        <div className="header-scores-score">
+          <div>Score</div>
+          <div>{score}</div>
+        </div>
+      </div>
     </div>
   );
 };

d45ed121df9fde9d30669e60ccec373a6cf49025
Mat Sz <contact@matsz.dev>
2020-09-29 18:00:09 +0200

    added a github corner

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import GithubCorner from 'react-github-corner';
 import './App.scss';
 
 import Home from './pages/Home';
@@ -8,6 +9,7 @@ const App: React.FC = () => {
   return (
     <Router>
       <div className="app">
+        <GithubCorner href="https://github.com/mat-sz/2048" />
         <Switch>
           <Route path="/">
             <Home />

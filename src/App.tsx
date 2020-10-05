39e980149b37badd50b98d1c182346b6ede27685
Mat Sz <contact@matsz.dev>
2020-10-05 18:01:33 +0200

    removed react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import GithubCorner from 'react-github-corner';
 import './App.scss';
 
-import Home from './pages/Home';
 import { animationDuration, gridGap } from './config';
+import Header from './components/Header';
+import Board from './components/Board';
+import Info from './components/Info';
+import BoardSizePicker from './components/BoardSizePicker';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div
-        className="app"
-        style={
-          {
-            '--animation-duration': animationDuration + 'ms',
-            '--grid-gap': gridGap,
-          } as any
-        }
-      >
-        <GithubCorner href="https://github.com/mat-sz/2048" />
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+    <div
+      className="app"
+      style={
+        {
+          '--animation-duration': animationDuration + 'ms',
+          '--grid-gap': gridGap,
+        } as any
+      }
+    >
+      <GithubCorner href="https://github.com/mat-sz/2048" />
+      <div className="page">
+        <Header />
+        <Board />
+        <BoardSizePicker />
+        <Info />
       </div>
-    </Router>
+    </div>
   );
 };
 
